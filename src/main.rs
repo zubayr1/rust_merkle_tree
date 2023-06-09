c93dcc223ba584788b3ea9ba02af96cff20ecebc
zubayr1 <write2zubayr@gmail.com>
2023-06-09 16:53:18 +0200

    create tree, get root

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,41 @@ fn hash(data: &[u8]) -> [u8; 32] {
     <[u8; 32]>::from(hasher.finalize_fixed())
 }
 
-
-fn main() {
-    println!("Hello, world!");
-
-    let leaf_values = ["a", "b", "c"];
-
+fn create_tree(leaf_values: Vec<String>) -> MerkleTree<Sha256>
+{
     let leaves: Vec<[u8; 32]> = leaf_values
         .iter()
         .map(|x| hash(x.as_bytes()))
         .collect();
-    
 
     let merkle_tree = MerkleTree::<Sha256>::from_leaves(&leaves);
 
+    return merkle_tree;
+}
+
+fn get_root(merkle_tree: MerkleTree<Sha256>) -> String
+{
     let root = merkle_tree.root_hex().ok_or("couldn't get the merkle root").unwrap();
 
+    return root;
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+
+
+    let mut leaf_values: Vec<String> = Vec::new();
+    leaf_values.push("a".to_string());
+    leaf_values.push("b".to_string());
+    leaf_values.push("b".to_string());
+
+    let merkle_tree = create_tree(leaf_values);
+
+    
+    let root = get_root(merkle_tree);
+
     println!("{}", root);
-    assert_eq!(
-        root,
-        "7075152d03a5cd92104887b476862778ec0c87be5c2fa1c0a90f87c49fad6eff".to_string()
-    );
+    
 }
